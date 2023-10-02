f4dc3f9385855090f0332c6f4781032e606f82cb
Marc0Franc0 <marcofranco340@gmail.com>
2023-10-02 20:50:57 -0300

    Creación de DiscoveryService
    -El mismo sirve para registrar cada uno de los microservicios mediante un nombre.
    -Cada microservicios tiene que ser cliente para ser registrado dentro del DiscoveryService

---
+++ DiscoveryService/src/main/java/com/DiscoveryService/DiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.DiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//Anotación necesaria para que funcione correctamente EurekaServer
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
