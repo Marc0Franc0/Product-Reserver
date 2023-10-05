285cf068438a20199de06460fee6087e7a7e9b6e
Marc0Franc0 <marcofranco340@gmail.com>
2023-10-05 01:38:10 -0300

    Se momentáneamente la seguridad en el ApiGateway

--- ApiGateway/src/main/java/com/ApiGateway/config/SecurityConfiguration.java
+++ ApiGateway/src/main/java/com/ApiGateway/config/SecurityConfiguration.java
@@ -1,24 +1,21 @@
-package com.config;
+package com.ApiGateway.config;
 
+/*import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebFluxSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain;*/
+//@Configuration
+//@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public SecurityWebFilterChain filter(ServerHttpSecurity httpSecurity) {
-         return httpSecurity
-                 .csrf(csrf->csrf.disable())
-                 .authorizeExchange(exchange ->exchange.anyExchange().authenticated())
-                 .oauth2Login(Customizer.withDefaults())
-                 .build();
-
+   /* @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
-
+*/
 }
